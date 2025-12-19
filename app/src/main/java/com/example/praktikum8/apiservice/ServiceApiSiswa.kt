acb03b1bf31c8d16d0ef08f2d43cbc652a8779f4
gibranrafii <gibranrafi21@gmail.com>
2025-12-19 10:06:46 +0700

    membuat service api untuk get / mengambil data

---
+++ app/src/main/java/com/example/praktikum8/apiservice/ServiceApiSiswa.kt
@@ -0,0 +1,11 @@
+package com.example.praktikum8.apiservice
+
+import com.example.praktikum8.modeldata.DataSiswa
+import retrofit2.http.Body
+import retrofit2.http.GET
+import retrofit2.http.POST
+
+interface ServiceApiSiswa {
+    @GET("bacaTeman.php")
+    suspend fun getSiswa(): List<DataSiswa>
+}
