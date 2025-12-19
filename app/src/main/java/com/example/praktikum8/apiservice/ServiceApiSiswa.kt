53740ee8620be9595ac8b4c207d0ef0b7ea80fbb
gibranrafii <gibranrafi21@gmail.com>
2025-12-19 10:07:01 +0700

    membuat service api untuk post / mengirim data

--- app/src/main/java/com/example/praktikum8/apiservice/ServiceApiSiswa.kt
+++ app/src/main/java/com/example/praktikum8/apiservice/ServiceApiSiswa.kt
@@ -8,4 +8,7 @@ import retrofit2.http.POST
 interface ServiceApiSiswa {
     @GET("bacaTeman.php")
     suspend fun getSiswa(): List<DataSiswa>
+
+    @POST("insertTM.php")
+    suspend fun postSiswa(@Body dataSiswa: DataSiswa):retrofit2.Response<Void>
 }
