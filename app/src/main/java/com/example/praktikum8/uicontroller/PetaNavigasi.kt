c8f9b0d5a1ab1742f15b5a26411e21ccf1002c66
gibranrafii <gibranrafi21@gmail.com>
2025-12-19 10:15:51 +0700

    membuat petanavigasi dan fungsi HostNavigasi

---
+++ app/src/main/java/com/example/praktikum8/uicontroller/PetaNavigasi.kt
@@ -0,0 +1,24 @@
+package com.example.praktikum8.uicontroller
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+
+@Composable
+fun DataSiswaApp(
+    navController: NavHostController = rememberNavController(),
+    modifier: Modifier
+) {
+    HostNavigasi(navController = navController)
+}
+
+@Composable
+fun HostNavigasi(
+    navController: NavHostController,
+    modifier: Modifier = Modifier
+) {
+
+}
