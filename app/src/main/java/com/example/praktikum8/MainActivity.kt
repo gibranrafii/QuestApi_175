31e5441f7c5488df6a84fe0177088e2d6233794b
gibranrafii <gibranrafi21@gmail.com>
2025-12-19 17:52:35 +0700

    memanggil fungsi di main

--- app/src/main/java/com/example/praktikum8/MainActivity.kt
+++ app/src/main/java/com/example/praktikum8/MainActivity.kt
@@ -12,6 +12,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.praktikum8.ui.theme.Praktikum8Theme
+import com.example.praktikum8.uicontroller.DataSiswaApp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,8 +21,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             Praktikum8Theme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    DataSiswaApp(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
