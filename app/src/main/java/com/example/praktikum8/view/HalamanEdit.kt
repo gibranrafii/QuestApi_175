ba152fad02d7ec4f2088178f0bab7876e143d0af
gibranrafii <gibranrafi21@gmail.com>
2025-12-26 18:12:20 +0700

    Revisi sedikit pada HalamanEdit.kt

--- app/src/main/java/com/example/praktikum8/view/HalamanEdit.kt
+++ app/src/main/java/com/example/praktikum8/view/HalamanEdit.kt
@@ -1,17 +1,17 @@
 package com.example.praktikum8.view
 
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import com.example.praktikum8.viewmodel.EditViewModel
-import androidx.lifecycle.viewmodel.compose.viewModel
-import com.example.praktikum8.viewmodel.provider.PenyediaViewModel
-import androidx.compose.material3.Scaffold
-import androidx.compose.ui.res.stringResource
-import androidx.compose.runtime.rememberCoroutineScope
-import kotlinx.coroutines.launch
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.praktikum8.uicontroller.route.DestinasiEdit
+import com.example.praktikum8.viewmodel.EditViewModel
+import com.example.praktikum8.viewmodel.provider.PenyediaViewModel
+import kotlinx.coroutines.launch
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -20,7 +20,7 @@ fun EditSiswaScreen(
     onNavigateUp: () -> Unit,
     modifier: Modifier = Modifier,
     viewModel: EditViewModel = viewModel(factory = PenyediaViewModel.Factory)
-){
+) {
     Scaffold(
         topBar = {
             SiswaTopAppBar(
@@ -30,11 +30,11 @@ fun EditSiswaScreen(
             )
         },
         modifier = modifier
-    ){ innerPadding ->
+    ) { innerPadding ->
         val coroutineScope = rememberCoroutineScope()
         EntrySiswaBody(
             uiStateSiswa = viewModel.uiStateSiswa,
-            onSiswaValueChange = viewModel::updateUIState,
+            onSiswaValueChange = viewModel::updateUiState,
             onSaveClick = {
                 coroutineScope.launch {
                     viewModel.editSatuSiswa()
