46cce58209dd699877c168ef124589e5e07db37d
gibranrafii <gibranrafi21@gmail.com>
2025-12-26 16:27:07 +0700

    melengkapi DestinasiEdit.kt

--- app/src/main/java/com/example/praktikum8/uicontroller/route/DestinasiEdit.kt
+++ app/src/main/java/com/example/praktikum8/uicontroller/route/DestinasiEdit.kt
@@ -2,5 +2,10 @@ package com.example.praktikum8.uicontroller.route
 
 import com.example.praktikum8.R
 
-object DestinasiEdit {
+object DestinasiEdit: DestinasiNavigasi {
+    override val route = "item_edit"
+    override val titleRes = R.string.edit_siswa
+
+    const val itemIdArg = "idSiswa"
+    val routeWithArgs = "$route/{$itemIdArg}"
 }
