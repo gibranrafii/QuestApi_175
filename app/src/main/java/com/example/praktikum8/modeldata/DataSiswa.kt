69babae2afbd7a6d675ca3a694e4ba2907af6b6a
gibranrafii <gibranrafi21@gmail.com>
2025-12-26 16:01:47 +0700

    Mengubah DataSiswa

--- app/src/main/java/com/example/praktikum8/modeldata/DataSiswa.kt
+++ app/src/main/java/com/example/praktikum8/modeldata/DataSiswa.kt
@@ -1,27 +1,27 @@
 package com.example.praktikum8.modeldata
 
+import com.example.praktikum8.modeldata.DetailSiswa
 import kotlinx.serialization.Serializable
+import kotlin.Int
 
 @Serializable
-data class DataSiswa(
+data class DataSiswa (
     val id : Int,
-    val nama : String,
-    val alamat : String,
-    val telpon : String
+    val nama: String,
+    val alamat: String,
+    val telpon: String
 )
-
 data class UIStateSiswa(
     val detailSiswa: DetailSiswa = DetailSiswa(),
     val isEntryValid: Boolean = false
 )
 
-data class DetailSiswa(
-    val id: Int = 0,
+data class DetailSiswa (
+    val id : Int = 0,
     val nama: String = "",
     val alamat: String = "",
     val telpon: String = ""
 )
-
 fun DetailSiswa.toDataSiswa(): DataSiswa = DataSiswa(
     id = id,
     nama = nama,
@@ -29,7 +29,7 @@ fun DetailSiswa.toDataSiswa(): DataSiswa = DataSiswa(
     telpon = telpon
 )
 
-fun DataSiswa.toUiStateSiswa(isEntryValid: Boolean = false): UIStateSiswa = UIStateSiswa(
+fun DataSiswa.toUIStateSiswa(isEntryValid: Boolean = false): UIStateSiswa = UIStateSiswa(
     detailSiswa = this.toDetailSiswa(),
     isEntryValid = isEntryValid
 )
