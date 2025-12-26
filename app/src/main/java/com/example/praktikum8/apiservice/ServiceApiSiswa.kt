e104c76fa17c489ff22b440b2b438ae32836f0fb
gibranrafii <gibranrafi21@gmail.com>
2025-12-26 16:00:03 +0700

    Menambah Get, put Delete

--- app/src/main/java/com/example/praktikum8/apiservice/ServiceApiSiswa.kt
+++ app/src/main/java/com/example/praktikum8/apiservice/ServiceApiSiswa.kt
@@ -2,13 +2,26 @@ package com.example.praktikum8.apiservice
 
 import com.example.praktikum8.modeldata.DataSiswa
 import retrofit2.http.Body
+import retrofit2.http.DELETE
 import retrofit2.http.GET
 import retrofit2.http.POST
+import retrofit2.http.PUT
+import retrofit2.http.Query
 
 interface ServiceApiSiswa {
-    @GET("bacaTeman.php")
-    suspend fun getSiswa(): List<DataSiswa>
+    @GET(value = "bacateman.php")
+    suspend fun getSiswa():List<DataSiswa>
 
-    @POST("insertTM.php")
+    @POST(value = "insertTM.php")
     suspend fun postSiswa(@Body dataSiswa: DataSiswa):retrofit2.Response<Void>
+
+    @GET("baca1Teman.php/{id}")
+    suspend fun getSatuSiswa(@Query("id") id: Int): DataSiswa
+
+    @PUT("editTM.php/{id}")
+    suspend fun editSatuSiswa(@Query("id") id: Int, @Body dataSiswa: DataSiswa): retrofit2.Response<Void>
+
+    @DELETE("deleteTM.php/{id}")
+    suspend fun hapusSatuSiswa(@Query("id") id: Int): retrofit2.Response<Void>
 }
+
