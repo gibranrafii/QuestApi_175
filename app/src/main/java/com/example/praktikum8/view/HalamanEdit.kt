bb3448ac062915371aa5b2cdb1cb14d04868bd26
gibranrafii <gibranrafi21@gmail.com>
2025-12-26 16:37:29 +0700

    menyelesaikan HalamanEdit.kt

--- app/src/main/java/com/example/praktikum8/view/HalamanEdit.kt
+++ app/src/main/java/com/example/praktikum8/view/HalamanEdit.kt
@@ -22,8 +22,26 @@ fun EditSiswaScreen(
     viewModel: EditViewModel = viewModel(factory = PenyediaViewModel.Factory)
 ){
     Scaffold(
-    ){
-
+        topBar = {
+            SiswaTopAppBar(
+                title = stringResource(DestinasiEdit.titleRes),
+                canNavigateBack = true,
+                navigateUp = onNavigateUp
+            )
+        },
+        modifier = modifier
+    ){ innerPadding ->
+        val coroutineScope = rememberCoroutineScope()
+        EntrySiswaBody(
+            uiStateSiswa = viewModel.uiStateSiswa,
+            onSiswaValueChange = viewModel::updateUIState,
+            onSaveClick = {
+                coroutineScope.launch {
+                    viewModel.editSatuSiswa()
+                    navigateBack()
+                }
+            },
+            modifier = Modifier.padding(innerPadding)
+        )
     }
-
 }
