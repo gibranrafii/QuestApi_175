f05d14c25232f2b3f47c3bf733138819ffd8497d
gibranrafii <gibranrafi21@gmail.com>
2025-12-26 16:33:04 +0700

    menambahkan fungsi CreationExtras

--- app/src/main/java/com/example/praktikum8/viewmodel/provider/PenyediaViewModel.kt
+++ app/src/main/java/com/example/praktikum8/viewmodel/provider/PenyediaViewModel.kt
@@ -1,19 +1,20 @@
 package com.example.praktikum8.viewmodel.provider
 
+import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.createSavedStateHandle
 import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
 import com.example.praktikum8.repositori.AplikasiDataSiswa
+import com.example.praktikum8.viewmodel.EditViewModel
 import com.example.praktikum8.viewmodel.EntryViewModel
-import com.example.praktikum8.viewmodeACl.HomeViewModel
+import com.example.praktikum8.viewmodel.HomeViewModel
 
-fun CreationExtras.aplikasiDataSiswa(): AplikasiDataSiswa =
-    (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as
-            AplikasiDataSiswa
-            )
-
+fun CreationExtras.aplikasiDataSiswa(): AplikasiDataSiswa = (
+        this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY]as
+                AplikasiDataSiswa
+        )
 object PenyediaViewModel {
     val Factory = viewModelFactory {
         initializer {
@@ -22,11 +23,6 @@ object PenyediaViewModel {
                     .repositoryDataSiswa
             )
         }
-        initializer {
-            EntryViewModel(
-                aplikasiDataSiswa().container
-                    .repositoryDataSiswa
-            )
-        }
+
     }
 }
