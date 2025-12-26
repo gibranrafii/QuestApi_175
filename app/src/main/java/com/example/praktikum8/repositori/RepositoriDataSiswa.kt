8bb7110f795d924e5f0039de99e1d11be2a12570
gibranrafii <gibranrafi21@gmail.com>
2025-12-26 16:04:16 +0700

    Menambahkan fungsi untuk get edit dan hapus

--- app/src/main/java/com/example/praktikum8/repositori/RepositoriDataSiswa.kt
+++ app/src/main/java/com/example/praktikum8/repositori/RepositoriDataSiswa.kt
@@ -7,12 +7,22 @@ import com.example.praktikum8.modeldata.DataSiswa
 interface RepositoryDataSiswa {
     suspend fun getDataSiswa(): List<DataSiswa>
     suspend fun postDataSiswa(dataSiswa: DataSiswa):retrofit2.Response<Void>
-}
+    //edit: tambahkan fungsi getSatuSiswa
+    suspend fun getSatuSiswa(id: Int): DataSiswa
+    suspend fun editSatuSiswa(id:Int, dataSiswa: DataSiswa):retrofit2.Response<Void>
+    suspend fun hapusSatuSiswa(id:Int):retrofit2.Response<Void>
 
+}
 class jaringanRepositoryDataSiswa (
     private val serviceApiSiswa: ServiceApiSiswa
 ): RepositoryDataSiswa{
     override suspend fun getDataSiswa(): List<DataSiswa> = serviceApiSiswa.getSiswa()
     override suspend fun postDataSiswa(dataSiswa: DataSiswa):retrofit2
     .Response<Void> = serviceApiSiswa.postSiswa(dataSiswa)
+    //edit:
+    override suspend fun getSatuSiswa(id: Int): DataSiswa = serviceApiSiswa.getSatuSiswa(id)
+    override suspend fun editSatuSiswa(id: Int, dataSiswa: DataSiswa):retrofit2.Response<Void> =
+        serviceApiSiswa.editSatuSiswa(id, dataSiswa)
+    override suspend fun hapusSatuSiswa(id: Int): retrofit2.Response<Void> =
+        serviceApiSiswa.hapusSatuSiswa(id)
 }
